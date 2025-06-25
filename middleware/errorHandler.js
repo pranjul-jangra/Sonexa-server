5538d99e8b69d4a11ca595d2d20a0cd64292103c
pranjul jangra <pranjuljan68@gmail.com>
2025-06-25 12:30:02 +0530

    Log error in middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,7 @@
 function errorHandler(err, req, res, next) {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
+    console.log("Error caught by middleware:", err);
 
     res.status(statusCode).json({
         success: false,
